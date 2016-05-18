60bf592d71357472afecf12b1608f874ce9567c1
Rosendo Galindo <rosendo_galindo@hotmail.com>
2016-05-18 01:46:08 -0500

    First Commit

---
+++ v1/datos/login_mysql.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Provee las constantes para conectarse a la base de datos
+ * Mysql.
+ */
+define("NOMBRE_HOST","localhost");
+define("BASE_DE_DATOS","people");
+define("USUARIO","root");
+define("CONTRASENA","nopodras");
